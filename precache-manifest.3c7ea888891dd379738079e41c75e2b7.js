ec1f892c15791b8861bb030592923f2eae21684c
DoLH <lehoangdo90@gmail.com>
2019-04-16 17:54:23 +0700

    Updates

---
+++ precache-manifest.3c7ea888891dd379738079e41c75e2b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "69cb38064edd4cdb071b",
+    "url": "/static/js/main.84f465d6.chunk.js"
+  },
+  {
+    "revision": "077cf1bed0c74f91a8cb",
+    "url": "/static/js/2.d379d675.chunk.js"
+  },
+  {
+    "revision": "69cb38064edd4cdb071b",
+    "url": "/static/css/main.b35e439c.chunk.css"
+  },
+  {
+    "revision": "077cf1bed0c74f91a8cb",
+    "url": "/static/css/2.8691967d.chunk.css"
+  },
+  {
+    "revision": "82551074e67e22348e93b45f9caffa44",
+    "url": "/index.html"
+  }
+];
